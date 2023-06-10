09ad29d831d932638b05eb11c42d5788a8db6fc6
Nomemb <nkj1205@gmail.com>
2023-06-10 15:24:51 +0900

    메인메뉴 보완

---
+++ Assets/Scripts/UI/BlinkImage.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class BlinkImage : MonoBehaviour
+{
+    [SerializeField] private Color highlightColor;
+    private float time;
+    private Image img;
+
+    private void Awake()
+    {
+        img = GetComponent<Image>();
+    }
+    void Update()
+    {
+        if(time<0.5f)
+        {
+            img.color = new Color(highlightColor.r, highlightColor.g, highlightColor.b, 1-time);
+        }
+        else
+        {
+            img.color = new Color(highlightColor.r, highlightColor.g, highlightColor.b, time);
+
+            if (time > 1f)
+                time = 0f;
+        }
+        time += Time.deltaTime;
+    }
+}
